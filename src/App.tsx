2ce9a89787d76c55918a78ab565ea9ef1c9a1096
chienlm <chienlm@fialda.vn>
2022-09-26 09:26:02 +0700

    cute

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,102 @@
 import * as React from 'react';
-import DefaultLayout from './layout';
+// import DefaultLayout from './layout';
 
 function App() {
-  return <DefaultLayout />;
+  // return <DefaultLayout />;
+  return <>
+    <div className='character'>
+      <div className="head">
+        <div className="head-head"></div>
+        <div className="head-mouth"></div>
+        <div className="ear-1"></div>
+        <div className="ear-2"></div>
+        <div className="eyelash-1"></div>
+        <div className="eyelash-2"></div>
+        <div className="eye-1"></div>
+        <div className="eye-2"></div>
+        <div className="shy-1"></div>
+        <div className="shy-2"></div>
+        <div className='nose'></div>
+        <div className="mouth"></div>
+        <div className="body-body"></div>
+        <div className="belly"></div>
+        <div className="left-arm"></div>
+        <div className="right-arm"></div>
+        <div className='wings'>
+          <div className="wrapper">
+            <div className="w">
+              <div className="line"></div>
+            </div>
+            <div className="w">
+              <div className="line"></div>
+            </div>
+            <div className="w">
+              <div className="line"></div>
+            </div>
+            <div className="w">
+              <div className="line"></div>
+            </div>
+            <div className="w">
+              <div className="line"></div>
+            </div>
+            <div className="w">
+              <div className="line"></div>
+            </div>
+            <div className="w">
+              <div className="line"></div>
+            </div>
+            <div className="w">
+              <div className="line"></div>
+            </div>
+            <div className="w">
+              <div className="line"></div>
+            </div>
+            <div className="w">
+              <div className="line"></div>
+            </div>
+          </div>
+
+          <div className="wrapper">
+            <div className="w">
+              <div className="line"></div>
+            </div>
+            <div className="w">
+              <div className="line"></div>
+            </div>
+            <div className="w">
+              <div className="line"></div>
+            </div>
+            <div className="w">
+              <div className="line"></div>
+            </div>
+            <div className="w">
+              <div className="line"></div>
+            </div>
+            <div className="w">
+              <div className="line"></div>
+            </div>
+            <div className="w">
+              <div className="line"></div>
+            </div>
+            <div className="w">
+              <div className="line"></div>
+            </div>
+            <div className="w">
+              <div className="line"></div>
+            </div>
+            <div className="w">
+              <div className="line"></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div className="motivation">
+      <h4>Thi tốt!</h4>
+    </div>
+    <div className="lucky-text"><h2>Nhunn</h2></div>
+    <div className='block'></div>
+  </>
 }
 
 export default App;
